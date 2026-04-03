27f82094fa6ad81b2086c638be6403100d0cfb68
juhyeon <dlwngus3856@naver.com>
2026-04-03 13:38:43 +0900

    [refactoring] ~51p

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,22 +3,33 @@ package baseball;
 public class Game {
 
     public void guess(String guessNumber) {
-        if(guessNumber == null) {
+        assertIllegalArgument(guessNumber);
+    }
+
+    private static void assertIllegalArgument(String guessNumber) {
+        if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
 
-        if(guessNumber.length() != 3){
+        if (guessNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
 
-        for(char number : guessNumber.toCharArray()){
-            if(number < '0' || number > '9'){
+        for (char number : guessNumber.toCharArray()) {
+            if (number < '0' || number > '9') {
                 throw new IllegalArgumentException();
             }
         }
 
-        if(guessNumber.charAt(0) == guessNumber.charAt(1) || guessNumber.charAt(0) == guessNumber.charAt(2) || guessNumber.charAt(1) == guessNumber.charAt(2)){
+        if (isDuplicatedNumber(guessNumber)) {
             throw new IllegalArgumentException();
         }
     }
+
+    private static boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2);
+    }
+
 }
